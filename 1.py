0535ae429306cce7db6722c63a534afe69a3138d
kunal <kunalkeshwani6@gmail.com>
2014-05-27 12:21:32 +0530

    function

--- 1.py
+++ 1.py
@@ -1,7 +1,14 @@
 #it's testing program
+
+def fun(s):
+	s+=" all is well"
+	print s
+
+s="chill dude"	
 for i in range(10):
 	if(i&1==1):
 		print "odd number is %d" %i
 for i in range(10):
-	print "just be happy"
+	print "just be happy"	
+fun(s)
 			 
